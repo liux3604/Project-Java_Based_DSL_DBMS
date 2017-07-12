f668ed47b39a9a780bd29d811ba0972aab588fc2
Michael Liu <liux3604@gmail.com>
2017-07-11 22:31:39 -0500

    come computation

--- Main_test.java
+++ Main_test.java
@@ -11,8 +11,36 @@ public class Main_test {
     private static final String EXIT   = "exit";
     private static final String PROMPT = "> ";
 
-    public static void main(String[] args) throws IOException {
-        Database db = new Database();
-        System.out.print(PROMPT);
+    public static void main(String[] args) {
+        double answer=0;
+        long sum=0;
+        for(int r=0;r<=8;r++){
+            for(int g=0;g<=8;g++){
+                for(int b=0;b<=8;b++){
+                    sum = sum + com(8,r)*com(8,g)*com(8,b);
+                }
+            }
+        }
+
+        answer= sum/512;
+
+        System.out.println(answer);
+
     }
+
+    public static long calc(long n) {
+        if (n <= 1)
+            return 1;
+        else
+            return n * calc(n - 1);
+    }
+
+    public static long com(long n, long a){
+        // calculates C(n,a)
+        long answer;
+        answer = calc(n)/(calc(n-a))/(calc(a));
+        return answer;
+    }
+
+
 }
